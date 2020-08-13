e88b1dba7a8661385a3ce639dd7b8392d37c5bd7
FriendDementor <vanninipablo@gmail.com>
2020-08-13 20:14:47 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # source: https://linoxide.com/tools/install-jira-on-ubuntu-linux/
 
-screen -d -m mysql bash /mysqlstart.sh
+screen -d -m bash -c /mysqlstart.sh
 sleep 5
 
 FILE=/mysqlConfigured.lock
-if ![ -f "$FILE" ]; then
+if [[ -f "$FILE" ]]; then
+    echo "SQL already configurated"
+else
     # first mysql configuration
     mysql -u root -p < config.sql
     echo "SQL configured"
@@ -13,12 +15,9 @@ if ![ -f "$FILE" ]; then
 fi
 sleep 5
 
-#FILE=/etc/init.d/jira
-#if ![ -f "$FILE" ]; then
-    ## install jira
-    #bash /installjira.sh
-#fi
-
 # jira installed & stopped
-screen -d -m jira bash /jirastart.sh
+#screen -d -m -t jira bash /jirastart.sh
+
+screen -d -m bash -c /entrypoint.sh
+
 read -p "Press enter to continue" -n 1
